6945845f6954d842894ede907ad0f31ae727e7a2
raminjafary <raminjafary1993@gmail.com>
2023-05-04 22:41:22 +0330

    initial commit

---
+++ core/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./common";
+export * from "./products";
+export * from "./cart";
---
+++ vue3-app/src/cart/pages/cart/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { CartPloc } from 'bloc-core'
+import { usePlocState } from '~/common/use-ploc-state'
+
+const ploc = inject<CartPloc>('cartPloc') as CartPloc
+const state = usePlocState(ploc)
+</script>
+
+<template>
+  <div class="text-3xl">
+    Cart Items
+    <router-link to="/products">
+      <VButtonText class="text-gray-300">Products</VButtonText>
+    </router-link>
+  </div>
+  <div class="max-w-4xl mx-auto my-10">
+    <div v-if="state.kind === 'LoadingCartState'">Loading...</div>
+    <div v-else-if="state.kind === 'UpdatedCartState'">
+      <div class="mb-10">
+        <div>Total {{ state.totalItems }} items</div>
+        <div>Total Price: {{ state.totalPrice }}</div>
+      </div>
+      <ul v-if="state.items.length > 0" class="flex flex-col space-y-8">
+        <li v-for="item in state.items" :key="item.id" class="flex items-center space-x-5">
+          <CartItem :item="item" />
+        </li>
+      </ul>
+      <div v-else>No Items Available</div>
+    </div>
+    <div v-else-if="state.kind === 'ErrorCartState'">Something went wrong!</div>
+  </div>
+</template>
